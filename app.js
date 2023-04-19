fd31afc58958d588776aa80b4a542d2d2fe75a7a
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-04-19 15:46:32 -0400

    second commit, working ripple feature

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//-------------------------------------Mouse Ripple----------------------------------------------//
+$("body").ripples({
+  resolution: 512,
+  dropRadius: 20,
+  perturbance: 0.01,
+});
+
+// $("body").ripples("drop", x, y, radius, strength);
+//-----------------------------------Submit Button------------------------------------------------//
 $("#submit").on("click", () => {
   $.get({
     url: `https://zenquotes.io/api/random`,
@@ -8,7 +17,7 @@ $("#submit").on("click", () => {
   });
 });
 
-//---------------------------------------------------------------------------------------//
+//------------------------------------Data Parsing---------------------------------------------------//
 
 function parseQuote(data) {
   console.log(data);
@@ -17,6 +26,7 @@ function parseQuote(data) {
     "https://random.dog/woof.json",
     function (dogData) {
       const imageUrl = dogData.url;
+      console.log(imageUrl);
       const zenQuote = data[0].h;
       const newCard = createQuoteCard(zenQuote, imageUrl);
       $(".card").append(newCard);
@@ -26,9 +36,16 @@ function parseQuote(data) {
 }
 
 function createQuoteCard(zenQuote, imageUrl) {
+  const extension = imageUrl.split(".").pop();
+  let mediaHtml;
+  if (extension === "mp4") {
+    mediaHtml = `<video controls><source src="${imageUrl}" type="video/mp4"></video>`;
+  } else {
+    mediaHtml = `<img src="${imageUrl}" class="card-img-top" alt="#" />`;
+  }
   const html = `
 <div class="card" style="width: 18rem">
-  <img src="${imageUrl}" class="card-img-top" alt="#" />
+  ${mediaHtml}
     <div class="card-body">
       <h5 class="card-title">Your Quote Is:</h5>
         <p class="card-text">
